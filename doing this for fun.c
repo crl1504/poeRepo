d04d012bea3f0a6499a1b5304b06e88292461455
crl1504 <82551601+crl1504@users.noreply.github.com>
2021-12-16 11:45:58 -0600

    Add files via upload

---
+++ doing this for fun.c
@@ -0,0 +1,24 @@
+
+/*
+  Project Title:
+  Team Members:
+  Date:
+  Section:
+
+
+  Task Description:
+
+
+  Pseudocode:
+
+*/
+
+task main()
+{                                     //Program begins, insert code within curly braces
+	int motorSpeed;
+	repeat(forever)
+
+
+
+
+}
